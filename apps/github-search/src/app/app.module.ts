0dfe6021d78fa7c9e7df5fa40d48a3a3102486c0
Balázs Tápai <balazs.tapai1990@gmail.com>
2020-10-22 15:21:21 +0200

    Added angular material to the project

--- apps/github-search/src/app/app.module.ts
+++ apps/github-search/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   bootstrap: [AppComponent]
 })
